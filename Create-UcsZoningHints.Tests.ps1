ee202abbf1430f17648d6d06d7e5d9387f8cc48b
tomtorggler <tomtorggler@users.noreply.github.com>
2016-08-07 21:02:24 +0200

    Updated parameters, tests

--- Create-UcsZoningHints.Tests.ps1
+++ Create-UcsZoningHints.Tests.ps1
@@ -4,23 +4,23 @@ $global:mockedUcsPsSession = [pscustomobject] @{
         Name = "My Demo UCS"
         Port = 443
     }
-$global:mockedUcsMSP = [Cisco.Ucsm.LsServer] @{
+$global:mockedUcsMSP = [pscustomobject] @{
     Name = "Server-1"
     Dn = "org-root/ls-Server-1"
     Rn = "ls-Server-1"
     SrcTemplName = "Server"
 }
-$global:mockedUcsCSP = [Cisco.UcsCentral.LsServer] @{
-    Name = "Server-1"
+$global:mockedUcsCSP = [pscustomobject] @{
+    Name = "Server-2"
     Dn = "org-root/ls-Server-1"
     Rn = "ls-Server-1"
     SrcTemplName = "Server"
 }
-$global:mockedUcsVHba = @(
+$global:mockedUcsMVHba = @(
     [pscustomobject] @{
         Name = "VHBA-A"
         Dn = "org-root/ls-Server-1/VHBA-A"
-        Rn = "fc-VHBA-B"
+        Rn = "fc-VHBA-A"
         Addr = "20:01:00:25:B5:00:1A:01"
         SwitchId = "A"
     }
@@ -32,15 +32,31 @@ $global:mockedUcsVHba = @(
         SwitchId = "B"
     }
 )
+$global:mockedUcsCVHba = @(
+    [pscustomobject] @{
+        Name = "VHBA-A"
+        Dn = "org-root/ls-Server-1/VHBA-A"
+        Rn = "fc-VHBA-A"
+        Addr = "20:01:00:25:B5:00:1A:11"
+        SwitchId = "A"
+    }
+    [pscustomobject] @{
+        Name = "VHBA-B"
+        Dn = "org-root/ls-Server-1/VHBA-B"
+        Rn = "fc-VHBA-B"
+        Addr = "20:01:00:25:B5:00:1B:11"
+        SwitchId = "B"
+    }
+)
 
 # Testing with mocked commands
 Describe "Testing Create-UcsZoningHints" {
-    
-    #Import-Module -Name Cisco.UCS.Core,Cisco.UCSCentral,Cisco.UCSManager -Global -ErrorAction Stop
             
     Mock Get-UcsPSSession -MockWith {return $global:mockedUcsPsSession} -verifiable
-    Mock Get-UcsServiceProfile -MockWith {return $global:mockedUcsSP} -Verifiable
-    Mock Get-UcsVhba -MockWith {return $global:mockedUcsVHba}
+    Mock Get-UcsServiceProfile -MockWith {return $global:mockedUcsMSP} -Verifiable
+    Mock Get-UcsCentralServiceProfile -MockWith {return $global:mockedUcsCSP} -Verifiable
+    Mock Get-UcsVhba -MockWith {return $global:mockedUcsMVHba}
+    Mock Get-UcsCentralVhba -MockWith {return $global:mockedUcsCVHba}
 
     $vsan = 12
     $ZoneSet = "cfg-prod"
@@ -56,7 +72,7 @@ Describe "Testing Create-UcsZoningHints" {
             $connected | Should Not Be $null
         }
     }
-    Context "Testing Output" {
+    Context "Testing Output for UCS Manager" {
         It "Test output to pipeline for Fabric A" {
             $output = Get-UcsServiceProfile | .\Create-UcsZoningHints.ps1 -Fabric A
             $output | select -First 1 | Should Match "! Fabric A.*"
@@ -108,4 +124,57 @@ Describe "Testing Create-UcsZoningHints" {
             (-join $output) | Should Match ".*zoneset activate name $ZoneSet vsan $vsan"
         }
     }
+    Context "Testing Output for UCS Central" {
+        It "Test output to pipeline for Fabric A" {
+            $output = Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -Fabric A
+            $output | select -First 1 | Should Match "! Fabric A.*"
+            $output | select -First 1 | Should Not Match "! Fabric B.*"
+            $output | select -ExpandProperty CommandLine | select -Last 1 | Should Match "! zoneset"
+        }
+        It "Test output to pipeline for Fabric B" {
+            $output = Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -Fabric B
+            $output | select -First 1 | Should Match "! Fabric B.*"
+            $output | select -First 1 | Should Not Match "! Fabric A.*"
+            $output | select -ExpandProperty CommandLine | select -Last 1 | Should Match "! zoneset"
+        }
+        It "Test output to pipeline for Fabric A with Parameters" {
+            $output = Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -Fabric A -ZoneSet $ZoneSet -Target $Target -vsan $vsan
+            $output | select -First 1 | Should Match "! Fabric A.*"
+            $output | select -First 1 | Should Not Match "! Fabric B.*"
+            $output | select -ExpandProperty CommandLine | select -Last 1 | Should Match "! zoneset activate name $ZoneSet vsan $vsan"
+        }
+        It "Test output to pipeline for Fabric B with Parameters" {
+            $output = Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -Fabric B -ZoneSet $ZoneSet -Target $Target -vsan $vsan
+            $output | select -First 1 | Should Match "! Fabric B.*"
+            $output | select -First 1 | Should Not Match "! Fabric A.*"
+            $output | select -ExpandProperty CommandLine | select -Last 1 | Should Match "! zoneset activate name $ZoneSet vsan $vsan"
+        }
+        It "Test output to file for Fabric A" {
+            $OutFile = Join-Path -Path $TestDrive -ChildPath "out.txt"
+            Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -OutFile $OutFile
+            $output = Get-Content -Path $OutFile
+            (-join $output) | Should Match "! Fabric A.*"
+        }
+        It "Test output to file for Fabric B" {
+            $OutFile = Join-Path -Path $TestDrive -ChildPath "out.txt"
+            Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -Fabric B -OutFile $OutFile
+            $output = Get-Content -Path $OutFile
+            (-join $output) | Should Match "! Fabric B.*"
+        }
+        It "Test output to file for Fabric A with Parameters " {
+            $OutFile = Join-Path -Path $TestDrive -ChildPath "out.txt"
+            Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -ZoneSet $ZoneSet -Target $Target -vsan $vsan -OutFile $OutFile
+            $output = Get-Content -Path $OutFile
+            (-join $output) | Should Match "^! Fabric A.*"
+            (-join $output) | Should Match ".*zoneset activate name $ZoneSet vsan $vsan"
+        }
+        It "Test output to file for Fabric B with Parameters " {
+            $OutFile = Join-Path -Path $TestDrive -ChildPath "out.txt"
+            Get-UcsCentralServiceProfile | .\Create-UcsZoningHints.ps1 -UcsCentral -Fabric B -ZoneSet $ZoneSet -Target $Target -vsan $vsan -OutFile $OutFile
+            $output = Get-Content -Path $OutFile
+            (-join $output) | Should Match "^! Fabric B.*"
+            (-join $output) | Should Match ".*zoneset activate name $ZoneSet vsan $vsan"            
+        }
+    }
+    Assert-VerifiableMocks
 }
